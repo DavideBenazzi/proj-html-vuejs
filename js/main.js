051dc1f8f2b35c2960e642a656fc4edbc791c441
Davide Benazzi <benazzi.davide@gmail.com>
2020-11-30 12:21:06 +0100

    Working on hero

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 // VUE 
 const app = new Vue ({
     el: '#app',
+    data: {
+        links: [
+            'Home',
+            'Who We Are',
+            'What We Do',
+            'Where We Work',
+            'Careers',
+            'News',
+        ],
+    },
 });
