ce6e5616ba174e8b66d8869b9b0c1130e4ebd551
Davide Benazzi <benazzi.davide@gmail.com>
2020-12-02 15:05:35 +0100

    Working on javascript

--- js/main.js
+++ js/main.js
@@ -11,5 +11,57 @@ const app = new Vue ({
             'News',
             'Get In Touch Now'
         ],
+        clients: {
+            img: [
+                './img/testimonials-2.jpg',
+                './img/testimonials-1.jpg',
+            ],
+            say: [
+                'Ability proceeds from a fusion of skills, knowledge, understanding and imagination, consolidated by experience.',
+                'Beauty is when you can appreciate yourself. When you love yourself, that\'s when you\'re most beautiful.',
+            ],
+            name: [
+                'Luis Desalvo',
+                'Shelia McCourtney',
+            ],
+            profession: [
+                'CREO TECH',
+                'ARCHITECT',
+            ],
+        },
+        clientActive: 0,
+        circleBackground1: 'selected',
+        circleBackground2: '',
+    },
+    created() {
+        // INTERVAL FOR SWITCH TESTIMONIALS
+        setInterval ( () => {
+            this.changeTestimonial();
+        }, 3000);
+    },
+    methods: {
+        // FUNCTION FOR SWAP TESTIMONIALS
+        changeTestimonial() {
+            if (this.clientActive === 0) {
+                this.clientActive = 1;
+                this.circleBackground1 = '';
+                this.circleBackground2 = 'selected';
+            } else {
+                this.clientActive = 0;
+                this.circleBackground1 = 'selected';
+                this.circleBackground2 = '';
+            };
+        },
+        // FUNCTIONS FOR SELECT TESTIMONIALS
+        selectTestimonial1() {
+            this.clientActive = 0;
+            this.circleBackground1 = 'selected';
+            this.circleBackground2 = '';
+        },
+        selectTestimonial2() {
+            this.clientActive = 1;
+            this.circleBackground1 = '';
+            this.circleBackground2 = 'selected';
+        },
     },
 });
